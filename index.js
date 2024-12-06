8c4b6a19bf711ed4d0564f0181c733c077791d9d
yuzu-and-blueberry <austinkitlee@outlook.com>
2024-12-05 16:27:35 -0800

    Did all the code for the main HTML CSS and JS logic. Project is pretty much complete with this commit, but it may be updated in the future for additional features.

---
+++ index.js
@@ -0,0 +1,45 @@
+/*
+1 meter = 3.281 feet
+1 liter = 0.264 gallon
+1 kilogram = 2.204 pound
+*/
+
+const convertEl = document.querySelector('#convertBtn')
+const inputEl = document.querySelector('#numInput')
+
+
+const lengthEl = document.querySelector('#lengthBox')
+const lengthResult = document.querySelector('#lengthresult')
+
+const volEl = document.querySelector('#volBox ')
+const volResult = document.querySelector('#volresult')
+
+const massEl = document.querySelector('#massBox')
+const massResult = document.querySelector('#massresult')
+
+convertEl.addEventListener('click', function() {
+    lengthConverter()
+    volumeConverter()
+    massConverter()
+})
+
+function lengthConverter() {
+    let inputNum = inputEl.value
+    let mToF = (inputNum * 3.281).toFixed(3)
+    let fToM = (inputNum / 3.281).toFixed(3)
+    lengthResult.textContent = `${inputNum} meters = ${mToF} feet | ${inputNum} feet = ${fToM} meters`
+}
+
+function volumeConverter() {
+    let inputNum = inputEl.value
+    let lToG = (inputNum * 0.264).toFixed(3)
+    let gToL = (inputNum / 0.264).toFixed(3)
+    volResult.textContent = `${inputNum} liters = ${lToG} gallons | ${inputNum} gallons = ${gToL} liters`
+}
+
+function massConverter() {
+    let inputNum = inputEl.value
+    let kToP = (inputNum * 2.204).toFixed(3)
+    let pToK = (inputNum / 2.204).toFixed(3)
+    massResult.textContent = `${inputNum} kilograms = ${kToP} pounds | ${inputNum} pounds = ${pToK} kilograms`
+}
